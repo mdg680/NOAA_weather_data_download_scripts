57a0981a029c331717229625523ef76b7c998892
mdg680 <monoper@gmail.com>
2017-11-17 14:51:39 +0100

    bash version 1 ready

--- getWeatherv2.sh
+++ getWeatherv2.sh
@@ -1,22 +1,31 @@
+
 counter=1
+counterMulti=0
+
+declare -a timestamps=("00" "06" "12" "18")
 
 data_root_path="https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/access/"
 
-while [ $counter -le 10 ]
+while [ $counter -le 30 ]
 do
     declare dd=$( date --date="-$counter days" +'%Y%m%d')
-    # cho $dd'1200'
     data_sub_path=$data_root_path$dd"120000-NCEI/0-fv02/"$dd"120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.0.nc"
-    echo $data_sub_path
-    wget $data_sub_path
+    dest_file_name="sst."$dd".grib2"
+    wget -nc -O $dest_file_name $data_sub_path
     ((counter++))
 done
 
-# wget https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/access/20170401120000-NCEI/0-fv02/2017041120000-NCEI-L4_GHRSSTblend-AVHRR_OI-GLOB-v02.0-fv02.0.nc
-# https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/access/20171106.120000NCEI/0-fv02/20171106120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.0.nc
-echo All done
+data_root_path="ftp://ftpprd.ncep.noaa.gov/pub/data/nccf/com/wave/prod/"
 
-# 
-# https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/access/20171114.120000NCEI/0-fv02/20171114120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.0.nc
+while [ $counterMulti -le 8 ]
+do
+    declare dd=$( date --date="-$counterMulti days" +'%Y%m%d')
+    for i in "${timestamps[@]}"
+    do
+	data_sub_path=$data_root_path"multi_1."$dd"/multi_1.nww3.t"$i"z.grib2"
+	dest_file_name="multi."$dd"."$i".grb2"
+	wget -nc -O $dest_file_name $data_sub_path
 
-# https://www.ncei.noaa.gov/data/sea-surface-temperature-optimum-interpolation/access/20170401120000-NCEI/0-fv02/20170401120000-NCEI-L4_GHRSST-SSTblend-AVHRR_OI-GLOB-v02.0-fv02.0.nc
+    done
+    ((counterMulti++))
+done
